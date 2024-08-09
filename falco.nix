b7a32991efb37df18cba79309228bcdda196a815
Fede Barcelona <fede_rico_94@hotmail.com>
2024-08-09 11:13:57 +0200

    refactor: move patches out of the nix file

--- falco.nix
+++ falco.nix
@@ -42,65 +42,6 @@ let
     hash = "sha256-pqDQf2tqtGn/f1WXEN+cRb2SAci4lGqykS7Rty7swCY=";
   };
 
-  disable-falcoctl-build-patch = writeText "disable-falcoctl-build-patch" ''
-    diff --git a/cmake/modules/falcoctl.cmake b/cmake/modules/falcoctl.cmake
-    index f462f552..f9ba3cf6 100644
-    --- a/cmake/modules/falcoctl.cmake
-    +++ b/cmake/modules/falcoctl.cmake
-    @@ -14,10 +14,15 @@
-
-     include(ExternalProject)
-
-    +option(ADD_FALCOCTL_DEPENDENCY "Add falcoctl dependency while building falco" ON)
-    +
-    +if(ADD_FALCOCTL_DEPENDENCY)
-     string(TOLOWER ''${CMAKE_HOST_SYSTEM_NAME} FALCOCTL_SYSTEM_NAME)
-
-     set(FALCOCTL_VERSION "0.8.0")
-
-    +message(STATUS "Building with falcoctl ''${FALCOCTL_VERSION}")
-    +
-     if(''${CMAKE_HOST_SYSTEM_PROCESSOR} STREQUAL "x86_64")
-         set(FALCOCTL_SYSTEM_PROC_GO "amd64")
-         set(FALCOCTL_HASH "7b763bfaf38faf582840af22750dca7150d03958a5dc47f6118748713d661589")
-    @@ -36,3 +41,6 @@ ExternalProject_Add(
-
-     install(PROGRAMS "''${PROJECT_BINARY_DIR}/falcoctl-prefix/src/falcoctl/falcoctl" DESTINATION "''${FALCO_BIN_DIR}" COMPONENT "''${FALCO_COMPONENT_NAME}")
-     install(DIRECTORY DESTINATION "''${FALCO_ABSOLUTE_SHARE_DIR}/plugins" COMPONENT "''${FALCO_COMPONENT_NAME}")
-    +else()
-    +    message(STATUS "Won't build with falcoctl")
-    +endif()
-  '';
-
-  allow-configure-falco-rules-dir = writeText "allow-configure-falco-rules-dir" ''
-    diff --git a/cmake/modules/rules.cmake b/cmake/modules/rules.cmake
-    index f62032ae..81a42946 100644
-    --- a/cmake/modules/rules.cmake
-    +++ b/cmake/modules/rules.cmake
-    @@ -15,6 +15,7 @@
-     include(GNUInstallDirs)
-     include(ExternalProject)
-
-    +if(NOT DEFINED FALCOSECURITY_RULES_FALCO_PATH)
-     # falco_rules.yaml
-     set(FALCOSECURITY_RULES_FALCO_VERSION "falco-rules-3.1.0")
-     set(FALCOSECURITY_RULES_FALCO_CHECKSUM "SHA256=3b617920c0b66128627613e591a954eb9572747a4c287bc13b53b38786250162")
-    @@ -28,10 +29,13 @@ ExternalProject_Add(
-       INSTALL_COMMAND ""
-       TEST_COMMAND ""
-     )
-    +endif()
-
-    +if(NOT DEFINED FALCOSECURITY_RULES_LOCAL_PATH)
-     # falco_rules.local.yaml
-     set(FALCOSECURITY_RULES_LOCAL_PATH "''${PROJECT_BINARY_DIR}/falcosecurity-rules-local-prefix/falco_rules.local.yaml")
-     file(WRITE "''${FALCOSECURITY_RULES_LOCAL_PATH}" "# Your custom rules!\n")
-    +endif()
-
-     if(NOT DEFINED FALCO_ETC_DIR)
-     	set(FALCO_ETC_DIR "''${CMAKE_INSTALL_FULL_SYSCONFDIR}/falco")
-  '';
-
   local-falco-rules = writeText "falco_rules.local.yaml" localFalcoRulesContent;
 
   drv = stdenv.mkDerivation rec {
@@ -141,8 +82,8 @@ let
     ];
 
     patches = [
-      disable-falcoctl-build-patch
-      allow-configure-falco-rules-dir
+      ./disable-falcoctl-build.patch
+      ./allow-configure-falco-rules-directory.patch
     ];
 
     KERNELDIR = "${kernel.dev}/lib/modules/${kernel.modDirVersion}/build";
